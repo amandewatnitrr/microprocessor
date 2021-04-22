1f6f4a8266e0f37fe86a02b30916d7234f8d885e
Aman Kumar Dewangan <52046981+amandewatnitrr@users.noreply.github.com>
2021-04-22 18:49:53 +0530

    Update subtract_2_16-bit_number.asm

--- subtract_2_16-bit_number.asm
+++ subtract_2_16-bit_number.asm
@@ -10,3 +10,14 @@ SUBB A, R0 ; Subtract the MSB of both the number
 MOV R1, A ; Transfer result from Accumulator to Register R1
 MOV 40H, R1 ; Store result in given memory location
 END
+
+/*Another Method*/
+ORG 0000H
+CLR C	  		;MAKE CY=0
+MOV A,#20H		;LOWER BYTE OF OPERAND 1 IN A (replace 20 with lower operand of your subtractend)
+SUBB A,#DEH	;SUBTRACT LOWER BYTE OF OPERAND 2 WITH A (replace DE with lower operand of your subtractor data)
+MOV R1,A		;STORES LSB OF RESULT IN R1
+MOV A,#65H		;HIGHER BYTE OF OPERAND 2 IN A  (replace 65 with higher operand of your subtractend)
+SUBB A,#ABH	;SUBTRACT WITH HIGHER BYTE OF OPERAND 1  (replace AB with higher operand of your subtractor)
+MOV R0,A		;STORES MSB OF RESULT IN R0
+END
