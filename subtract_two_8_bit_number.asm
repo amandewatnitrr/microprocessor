e9a860554dfb213d88ddefe4771de6220f2fa6e1
Aman Kumar Dewangan <52046981+amandewatnitrr@users.noreply.github.com>
2021-04-22 18:40:24 +0530

    Update subtract_two_8_bit_number.asm

--- subtract_two_8_bit_number.asm
+++ subtract_two_8_bit_number.asm
@@ -4,3 +4,23 @@ MOV R2, #30H
 MOV A, R1
 SUBB A, R2
 END
+
+/*Another method*/
+
+ORG 000H
+MOV R0,#20H;set  20H to R0
+MOV R1,#30H;set destination address 30H to R1
+MOV A,R0;take the value from source to register A
+MOV R5,A; Move the value from A to R5
+MOV R4,#00H; Clear register R4 to store borrow
+MOV A,R0; take the value from source to register A
+MOV R3,A; store second byte
+MOV A,R5;get back the first operand
+SUBB A,R1; Subtract R3 from A
+JNC SAVE
+INC R4; Increment R4 to get borrow
+MOV B,R4;Get borrow to register B
+MOV R1,B; Store the borrow first
+
+SAVE:  MOV R1,A; Store the result
+END
