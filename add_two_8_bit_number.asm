c45c966b5822c8ffacedd44274de93fba8dee5a7
Aman Kumar Dewangan <52046981+amandewatnitrr@users.noreply.github.com>
2021-04-22 16:05:11 +0530

    Update add_two_8_bit_number.asm

--- add_two_8_bit_number.asm
+++ add_two_8_bit_number.asm
@@ -1,5 +1,5 @@
 ORG 0000H
-MOV A,#45H
-MOV B,#86H
-ADD A,B
-END
+MOV A,#45H ; Moving Hexadeciaml Data to the Accumulator
+MOV B,#86H ; Moving Hexadecimal data to register B
+ADD A,B ; The Addition of A and B is updated in the Accumulator A the same statement can aslo be written as (ADD B)
+END ; Ends the program
